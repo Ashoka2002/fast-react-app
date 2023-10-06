f59e4b02540e36dff451b321452f726467e69ae2
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-06 20:33:12 +0530

    new router version 6 & adding starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  const title = "React";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-  return <h1>Vite + {title}</h1>;
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/:orderId", element: <Order /> },
+  { path: "/order/new", element: <CreateOrder /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
