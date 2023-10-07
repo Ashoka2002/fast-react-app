2e3bc58ee42cbc56ecf186ea2693d85ed399673b
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-07 08:51:20 +0530

    applayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast-React-Pizza Co.</Link>
+      <h3>Ashok.</h3>
+    </header>
+  );
+}
+
+export default Header;
