2237b5c526cfa47486b58d73dcc6bc4277fd12bf
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-08 18:15:53 +0530

    user state

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="hidden text-xs font-semibold sm:block">Ashok.</div>;
+  const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-xs font-semibold sm:block">{userName}</div>
+  );
 }
 
 export default UserName;
