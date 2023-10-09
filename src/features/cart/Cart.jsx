a248c2ed269d0cbceb73902c3d7d40c0c3fc0a0c
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-09 09:14:01 +0530

    cart logic

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,37 +1,20 @@
-import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "../cart/CartItem";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
   const userName = useSelector((state) => state.user.userName);
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
 
-  const cart = fakeCart;
+  function handleClearCart() {
+    if (window.confirm("Do you want to clear cart?")) dispatch(clearCart());
+  }
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="p-3">
@@ -49,7 +32,10 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+
+        <Button type="secondary" onClick={handleClearCart}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
