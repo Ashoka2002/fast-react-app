12a69e2e5db385a8784fa66e7f6fa66a54198ed1
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-29 16:56:36 +0530

    converting currency

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,10 @@
+const INRPRICE = 10;
+
 export function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
-    currency: "EUR",
-  }).format(value);
+    currency: "INR",
+  }).format(value * INRPRICE);
 }
 
 export function formatDate(dateStr) {
